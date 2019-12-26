131a41744819af65a2821ccd1147989a086a5328
Alexander <ga86jej@mytum.de>
2019-12-26 21:54:28 +0100

    finished day 10

--- 2019/src/days/Day10.java
+++ 2019/src/days/Day10.java
@@ -2,41 +2,103 @@ package days;
 
 import java.util.*;
 
-
 public class Day10 {
     public static void main(String[] args) {
-        char[][] asteriodField = scanAsteriodField();
+        char[][] asteroidField = scanAsteriodField();
         
-        partOne(asteriodField);
+        Asteroid bestAsteroid = partOne(asteroidField);
+        partTwo(bestAsteroid, asteroidField);
     }
 
-    public static void partOne(char[][] asteriodField) {
-        int maxObservableAsteroids = -1;
-        int observableAsteroids = -1;
-        for (int i = 0; i < asteriodField.length; i++)
-            for (int j = 0; j < asteriodField[i].length; j++) {
-                if (asteriodField[i][j] == '#' && 
-                    (observableAsteroids = determineObservableAsteroids(i, j, asteriodField)) > maxObservableAsteroids) {
-                    maxObservableAsteroids = observableAsteroids;
+    public static Asteroid partOne(char[][] asteroidField) {
+        Asteroid bestAsteroid = null;
+        for (int i = 0; i < asteroidField.length; i++)
+            for (int j = 0; j < asteroidField[i].length; j++) {
+                if (asteroidField[i][j] == '#') {
+                    Asteroid asteroid = new Asteroid(i, j);
+                    asteroid.determineObservableAsteroids(asteroidField);
+                    if (bestAsteroid == null || bestAsteroid.observableAsteroids < asteroid.observableAsteroids)
+                        bestAsteroid = asteroid;
                 }
             }
-        System.out.println(maxObservableAsteroids);
+        System.out.println(bestAsteroid.observableAsteroids);
+        return bestAsteroid;
     }
 
-    public static int determineObservableAsteroids(int x, int y, char[][] asteriodField) {
-        int observableAsteroids = 0;
-        List<Vector<Integer>> blockedVectors = new ArrayList<>();
-        for (int i = 0; i < asteriodField.length; i++)
-            for (int j = 0; j < asteriodField[i].length; j++) {
-                if (asteriodField[i][j] == '#' && !(i == x && j == y)) {
-                    Vector<Integer> minVector = calculateMinVector(i - x, j - y);
-                    if (!blockedVectors.contains(minVector)) {
-                        blockedVectors.add(minVector);
-                        observableAsteroids++;
-                    }
+    // TODO Part two needs a rework (simplify used datastructures maybe?)
+    public static void partTwo(Asteroid laserAsteroid, char[][] asteroidField) {
+        Map<Double, List<Vector<Integer>>> angleAsteroids = getAngleAsteroids(laserAsteroid, asteroidField);
+        sortAngleAsteroids(angleAsteroids);
+        Vector<Integer> lastAsteroid = getLastAsteroid(angleAsteroids);
+        System.out.println((lastAsteroid.get(0)+laserAsteroid.x)*100 + (lastAsteroid.get(1)+laserAsteroid.y));
+        
+    }
+
+    public static Vector<Integer> getLastAsteroid(Map<Double, List<Vector<Integer>>> angleAsteroids) {
+        List<List<Vector<Integer>>> angleAsteroidsList = new ArrayList<>(angleAsteroids.values());
+        int asteroidsToDestroy = 200;
+        Vector<Integer> lastAsteroid = null;
+        int i = 0;
+        for (Double ang: angleAsteroids.keySet())
+            if (ang < 90)
+                i++;
+        while (!angleAsteroidsList.isEmpty()){
+            if (i <= 0 )
+                i = angleAsteroidsList.size() - 1;
+            List<Vector<Integer>> list = angleAsteroidsList.get(i);
+            asteroidsToDestroy--;
+            if (asteroidsToDestroy == 1) {
+                lastAsteroid = list.get(0);
+                break;
+            }
+            list.remove(0);
+            if (list.isEmpty())
+                angleAsteroidsList.remove(list);
+            i--;
+        }
+        return lastAsteroid;
+    }
+
+    public static double calculateAngle(int x, int y) {
+        return ((Math.atan2(-y, x) / Math.PI * 180) +  360) % 360;
+    }
+
+    public static Map<Double, List<Vector<Integer>>> 
+                getAngleAsteroids(Asteroid laserAsteroid, char[][] asteroidField) {
+        Map<Double, List<Vector<Integer>>> angleAsteroids = new TreeMap<>();
+        for (int i = 0; i < asteroidField.length; i++)
+            for (int j = 0; j < asteroidField[i].length; j++) {
+                if (asteroidField[i][j] == '#') {
+                    Vector<Integer> asteroid = new Vector<>();
+                    int xRelative = i - laserAsteroid.x;
+                    int yRelative = j - laserAsteroid.y;
+                    asteroid.add(xRelative);
+                    asteroid.add(yRelative);
+                    double angle = calculateAngle(xRelative, yRelative);
+                    List<Vector<Integer>> asteroidsAtAngle;
+
+                    if (angleAsteroids.containsKey(angle))
+                        asteroidsAtAngle = angleAsteroids.get(angle);
+                    else
+                        asteroidsAtAngle = new ArrayList<>();
+
+                    asteroidsAtAngle.add(asteroid);
+                    angleAsteroids.put(angle, asteroidsAtAngle);
                 }
             }
-        return observableAsteroids;
+        return angleAsteroids;
+    }
+
+    public static void sortAngleAsteroids(Map<Double, List<Vector<Integer>>> angleAsteroids) {
+        for (List<Vector<Integer>> l: angleAsteroids.values()) 
+            Collections.sort(l, new Comparator<Vector<Integer>>() {
+                public int compare(Vector<Integer> v1, Vector<Integer> v2) {
+                    double distanceV1 = Math.sqrt(Math.pow(v1.get(0), 2) + Math.pow(v1.get(0), 2));
+                    double distanceV2 = Math.sqrt(Math.pow(v2.get(0), 2) + Math.pow(v2.get(0), 2));
+                    // vectors will always be distinct so there is no need to check for equality
+                    return (distanceV1 - distanceV2 > 0) ? 1 : -1; 
+                }
+            });
     }
 
     public static Vector<Integer> calculateMinVector(int x, int y) {
@@ -70,12 +132,48 @@ public class Day10 {
 
         // Transpose matrix to get coordinates right, maybe refactor later to a more 
         // efficient solution
-        for (int i = 0; i < asteriodField.length; i++)
-            for (int j = i + 1; j < asteriodField[i].length; j++) {
-                char tmp = asteriodField[i][j];
-                asteriodField[i][j] = asteriodField[j][i];
-                asteriodField[j][i] = tmp;
+        char[][] transposedField = new char[asteriodField[0].length][];
+        for (int i = 0; i < asteriodField[0].length; i++) {
+            transposedField[i] = new char[asteriodField.length];
+            for (int j = 0; j < asteriodField.length; j++) {
+                transposedField[i][j] = asteriodField[j][i];
             }
-        return asteriodField;
+        }
+        return transposedField;
+    }
+
+    public static class Asteroid {
+        public int x;
+        public int y;
+
+        public int observableAsteroids = 0;
+
+        public Asteroid(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public String toString(){
+            return this.x + ":" + this.y + ", oA: " + this.observableAsteroids;
+        } 
+
+        public void incrementObservableAsteroids() {
+            this.observableAsteroids++;
+        }
+
+        public void determineObservableAsteroids(char[][] asteriodField) {
+            List<Vector<Integer>> blockedVectors = new ArrayList<>();
+            for (int i = 0; i < asteriodField.length; i++)
+                for (int j = 0; j < asteriodField[i].length; j++) {
+                    if (asteriodField[i][j] == '#' && !(i == this.x && j == this.y)) {
+                        Vector<Integer> minVector = calculateMinVector(i - this.x, j - this.y);
+                        if (!blockedVectors.contains(minVector)) {
+                            blockedVectors.add(minVector);
+                            this.observableAsteroids++;
+                        }
+                    }
+                }
+        }
+
     }
 }
